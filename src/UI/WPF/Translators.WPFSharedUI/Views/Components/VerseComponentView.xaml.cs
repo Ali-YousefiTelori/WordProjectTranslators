6ea87308e357dcef5dfd479ec0db2b973ef63a0a
Ali Yousefi <ali.visual.studio@gmail.com>
2022-10-27 18:23:15 +0330

    Support for net 6 #12

--- src/UI/WPF/Translators.WPFSharedUI/Views/Components/VerseComponentView.xaml.cs
+++ src/UI/WPF/Translators.WPFSharedUI/Views/Components/VerseComponentView.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Windows.Controls;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Translators.UI.Views.Components
 {
