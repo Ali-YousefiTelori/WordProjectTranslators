df88d66f286974c5494f3acc373047b86e2e53d2
Ali Yousefi <ali.visual.studio@gmail.com>
2022-10-27 18:16:30 +0330

    Support for WPF #12

---
+++ src/UI/WPF/Translators.WPFSharedUI/Views/Components/BusyComponentView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Translators.UI.Views.Components
+{
+    public sealed partial class BusyComponentView : UserControl
+    {
+        public BusyComponentView()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IsRunningProperty = DependencyProperty.Register("IsRunning", typeof(bool), typeof(BusyComponentView), new PropertyMetadata(new PropertyChangedCallback(Changed)));
+
+        private static void Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is bool value && value)
+            {
+                (d as BusyComponentView).Visibility = Visibility.Visible;
+            }
+            else
+                (d as BusyComponentView).Visibility = Visibility.Collapsed;
+        }
+
+        public bool IsRunning
+        {
+            get { return (bool)GetValue(IsRunningProperty); }
+            set { SetValue(IsRunningProperty, value); }
+        }
+    }
+}
---
+++ src/UI/WPF/Translators.WPFSharedUI/Views/Components/VerseComponentView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Translators.UI.Views.Components
+{
+    public sealed partial class VerseComponentView : UserControl
+    {
+        public VerseComponentView()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SourceViewModelProperty = DependencyProperty.Register("SourceViewModel", typeof(object), typeof(VerseComponentView), new PropertyMetadata());
+
+        public object SourceViewModel
+        {
+            get { return (object)GetValue(SourceViewModelProperty); }
+            set { SetValue(SourceViewModelProperty, value); }
+        }
+    }
+}
